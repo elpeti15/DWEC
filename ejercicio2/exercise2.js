826678adc22d314ad441adb2ef4e711fba9640fb
elpeti15 <miguelpf.bg@gmail.com>
2024-10-19 00:15:10 +0200

    ejercicio 2 con template

--- ejercicio2/exercise2.js
+++ ejercicio2/exercise2.js
@@ -117,49 +117,22 @@ function validateImage(){
 
 //Añadimos restaurante
 function addRestaurant(){
+
     const contenedor = document.getElementById("placesContainer");
-    
-    const colDiv = document.createElement("div");
-    colDiv.classList.add("col");
-    contenedor.append(colDiv);
+    const template = document.getElementById("template");
 
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
-    cardDiv.classList.add("h-100");
-    cardDiv.classList.add("shadow");
-    colDiv.append(cardDiv);
+    //Clonamos el contenido de la plantilla
+    const htmlTemplate = template.content.cloneNode(true);
 
-    const img = document.createElement("img");
-    img.classList.add("card-img-top");
+    const img = htmlTemplate.querySelector("img");
     img.src = imgPreview.src;  // se usa la de imgPreview
-    cardDiv.append(img);
-
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    cardDiv.append(cardBody);
-
-    const h4 = document.createElement("h4");
-    h4.classList.add("card-title");
-    h4.textContent = f.name.value;
-    cardBody.append(h4);
-
-    const p = document.createElement("p");
-    p.classList.add("card-text");
-    p.textContent = f.description.value;
-    cardBody.append(p);
-
-    const textDiv1 = document.createElement("div");
-    textDiv1.classList.add("card-text");
-    cardBody.append(textDiv1);
-
-    const small1 = document.createElement("small");
-    small1.classList.add("text-muted");
-    textDiv1.append(small1);
-
-    const strong1 = document.createElement("strong");
-    strong1.textContent = "Apertura: "
-    small1.append(strong1);
     
+    const h4 = htmlTemplate.querySelector("h4");
+    h4.textContent = f.name.value;
+
+    const p = htmlTemplate.querySelector("p");
+    p.textContent = f.description.value;
+
     //Usamos array auxiliar
     const weekDays = ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"];
     const today = new Date().getDay();
@@ -168,45 +141,23 @@ function addRestaurant(){
         .filter((d) => d.checked)
         .map((d) => weekDays[d.value]);
         
-    small1.append(selectedDays.join(", "));
+    const strong1 = htmlTemplate.querySelector("strong");
+    strong1.textContent = "Apertura: ";
+    strong1.after(selectedDays.join(", "));
 
-    const openSpan = document.createElement("span");
-    openSpan.classList.add("badge");
-    openSpan.classList.add("ms-2");
-    openSpan.classList.add("bg-success");
-    openSpan.textContent = "Abierto";
-
-    const closedSpan = document.createElement("span");
-    closedSpan.classList.add("badge");
-    closedSpan.classList.add("ms-2");
-    closedSpan.classList.add("bg-danger");
-    closedSpan.textContent = "Cerrado";
+    const spans = htmlTemplate.querySelectorAll("span");
 
     if (selectedDays.indexOf(weekDays[today]) !== -1){
-        small1.append(openSpan);
+        spans[1].classList.add("d-none");
     } else{
-        small1.append(closedSpan);
+        spans[0].classList.add("d-none");
     }
 
-    const textDiv2 = document.createElement("div");
-    textDiv2.classList.add("card-text");
-    cardBody.append(textDiv2);
+    const strong2 = htmlTemplate.querySelectorAll("strong")[1];
+    strong2.after(f.phone.value);
 
-    const small2 = document.createElement("small");
-    small2.classList.add("text-muted");
-    textDiv2.append(small2);
+    const small = htmlTemplate.querySelectorAll("small")[2];
+    small.textContent = f.cuisine.value;
 
-    const strong2 = document.createElement("strong");
-    strong2.textContent = "Teléfono: ";
-    small2.append(strong2);
-    small2.append(f.phone.value);
-
-    const footerDiv = document.createElement("div");
-    footerDiv.classList.add("card-footer");
-    cardDiv.append(footerDiv);
-
-    const small3 = document.createElement("small");
-    small3.classList.add("text-muted");
-    small3.textContent = f.cuisine.value;
-    footerDiv.append(small3);
+    contenedor.append(htmlTemplate);
 }
