d458726556711464082756b02de6073eb1896594
elpeti15 <miguelpf.bg@gmail.com>
2025-06-02 12:43:50 +0200

    cambios en config

--- Unidad 3/ionic-physiocare/capacitor.config.ts
+++ Unidad 3/ionic-physiocare/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionic-physiocare',
-  webDir: 'www/browser',
+  webDir: 'www',
   android: {
     allowMixedContent: true
   },
