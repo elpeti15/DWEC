cdb2aa7e721d37cd17866c99b71e66c814b019f1
elpeti15 <miguelpf.bg@gmail.com>
2025-06-05 16:05:32 +0200

    adaptamos status bar

--- Unidad 3/ionic-physiocare/capacitor.config.ts
+++ Unidad 3/ionic-physiocare/capacitor.config.ts
@@ -7,6 +7,13 @@ const config: CapacitorConfig = {
   android: {
     allowMixedContent: true
   },
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      style: "DARK",
+      backgroundColor: "#0054e9",
+    },
+  }
 };
 
 export default config;
